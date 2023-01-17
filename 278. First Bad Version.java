f926591188a977fedacc7cd7f374f5b26ec9f682
Saksham Bhargava <sakshambhargava@yahoo.com>
2023-01-17 23:29:33 +0530

    Create 278. First Bad Version.java

---
+++ 278. First Bad Version.java
@@ -0,0 +1,18 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int low=1,high=n,mid;
+        int ans=-1;
+        while(low<=high)
+        {
+            mid = low + ((high - low)/2 );
+            if(isBadVersion(mid))
+            {
+                high=mid-1;
+                ans=mid;
+            }
+            else
+                low=mid+1;
+        }
+        return ans;
+    }
+}
